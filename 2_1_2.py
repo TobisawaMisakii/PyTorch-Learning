c6d26b90939e90863d806c3ef68ad2b849cf4eab
Saito <116051090+TobisawaMisakii@users.noreply.github.com>
2024-04-25 11:20:30 +0800

    Create 2_1_2.py

---
+++ 2_1_2.py
@@ -0,0 +1,27 @@
+import torch
+import torch as t
+import math
+
+#element-wise calculation
+x = t.tensor([1.5, 3, -1, 0])
+y = t.tensor([-2, 1, 2, 5])
+print(x+y, end='\n')
+print(x-y, end='\n')
+print(x*y, end='\n')
+print(x/y, end='\n')
+print(x**y, end='\n')
+print(t.exp(y), end='\n')
+Y = t.zeros(y.shape)
+t.exp(y, out = Y)
+print(Y)
+
+print(t.exp(y) == Y)
+
+#concat
+x = t.arange(12, dtype=float).reshape(3,4)
+y = t.randn(12, dtype=float).reshape(3,4)
+print(x, y, end="\n")
+print(t.cat((x,y), dim=0), end='\n')
+print(t.cat((x,y), dim=1), end='\n')
+
+print(x.sum())
